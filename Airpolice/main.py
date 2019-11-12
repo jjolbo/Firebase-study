6d6cdfcf72a625b8d9b7a2fa09a36df9a9184a4c
jjolbo <jeongha3751@kookmin.ac.kr>
2019-11-12 19:37:13 +0900

    Run code automatically for a certain time

--- Airpolice/main.py
+++ Airpolice/main.py
@@ -2,25 +2,32 @@ import threading
 import openApi_call
 import weather_api
 import kFirebase
-
+import worst_air
+import check_worst
 # end = False
 
-def _main(secode = 1.0):
-    # global end
-    # if end:
-    #     return
-
-    Firebase = kFirebase.Firebase()
-    Firebase.firebase_db()
-
+def _outside(second = 1800.0):
     Weather = weather_api.WeatherAPI()
     Mise = openApi_call.MiseAPI()
 
     Weather._main()
     Mise._main()
 
-    # threading.Timer(second, _main, [second]).start()
+    threading.Timer(second, _outside, [second]).start()
+
+def _inside(second=3.0):
+    Check_mise = worst_air.Check_mise()
+    Check_worst = check_worst.Check_worst()
+
+    # Check_mise._main()
+    Check_worst._main()
+
+    threading.Timer(second, _inside, [second]).start()
 
 
 if __name__ == '__main__':
-    _main()
+    Firebase = kFirebase.Firebase()
+    Firebase.firebase_db()
+
+    _outside()
+    _inside()
