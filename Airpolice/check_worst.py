61f0e09041118e19df1b6dc04d2832cfb34bb918
jjolbo <jeongha3751@kookmin.ac.kr>
2019-11-12 22:07:03 +0900

    Modify the algorithm to check the worst air area

--- Airpolice/check_worst.py
+++ Airpolice/check_worst.py
@@ -1,5 +1,6 @@
 import kFirebase
 import datetime
+import numpy as np
 
 class Check_worst:
     def date_time(self):
@@ -17,6 +18,16 @@ class Check_worst:
 
         return month, day, hour, min
 
+    def find_max(self, list):
+        Firebase = kFirebase.Firebase()
+        # print(list)
+        for idx in range(len(list)):
+            if list[idx] == max(list):
+                # print('findmax',idx)
+                Firebase.wa_update('mode/', {
+                    'auto_area': idx
+                })
+
     def worst_air(self):
         month, day, hour, min = self.date_time()
 
@@ -24,28 +35,52 @@ class Check_worst:
         # Firebase.firebase_db()
         area = ['kitchen', 'livingroom', 'room']
 
-        val_list = []
+        pm10_list = []
+        pm25_list = []
 
         for i in range(3):
             pm10path = 'inside/' + area[i] + '/' + str(month) + str(day) + '/' + str(hour) + ':' + str(min) + '/' + 'pm10Value'
             pm10value = Firebase.load(pm10path)
-            val_list.append(pm10value)
+            pm10_list.append(pm10value)
 
-        if (val_list[0] == val_list[1] or val_list[1] == val_list[2] or val_list[0] == val_list[2] or (val_list[0]==val_list[1]and val_list[1] == val_list[2])):
-            val_list.clear()
-            for i in range(3):
-                pm25path = 'inside/' + area[i] + '/' + str(month) + str(day) + '/' + str(hour) + ':' + str(
-                    min) + '/' + 'pm25Value'
-                pm25value = Firebase.load(pm25path)
-                val_list.append(pm25value)
+            pm25path = 'inside/' + area[i] + '/' + str(month) + str(day) + '/' + str(hour) + ':' + str(
+                min) + '/' + 'pm25Value'
+            pm25value = Firebase.load(pm25path)
+            pm25_list.append(pm25value)
 
-        print(val_list)
-        for idx in range(len(val_list)):
-            if val_list[idx] == max(val_list):
-                print(idx)
-                Firebase.wa_update('mode/', {
-                    'auto_area': idx
-                })
+        # worst: 3, bad: 2, normal: 1, good: 0
+        auto_area = [0,0,0]
+
+        print(pm10_list)
+        print(pm25_list)
+
+        for i in range(3):
+            if pm10_list[i] >= 101 or pm25_list[i] >= 51:
+                auto_area[i] = 3
+            elif pm10_list[i] >= 151 or pm25_list[i] >= 26:
+                auto_area[i] = 2
+            elif pm10_list[i] >= 31 or pm25_list[i] >= 16:
+                auto_area[i] = 1
+            else:
+                auto_area[i] = 0
+
+        print(auto_area)
+
+        pm10_array = np.array(pm10_list)
+        pm25_array = np.array(pm25_list)
+
+        if auto_area.count(1) > 1 or auto_area.count(2) > 1 or auto_area.count(3) > 1:
+            self.find_max(pm25_array + pm10_array)
+            return
+
+        if sum(auto_area) == 0:
+            print(1)
+            Firebase.wa_update('mode/', {
+                'auto_area': 1
+            })
+        else:
+            self.find_max(auto_area)
+
 
     def _main(self):
         self.worst_air()
